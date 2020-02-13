fe614ea91e30e2fdc9727b3bdefbaa3bc80d0335
Fgp910 <f.gutierrezpena910@gmail.com>
2020-02-14 00:23:19 +0100

    Lo de los hilos funciona, pero no sé si bien. Valgrind no llora

--- ejercicio_hilos.c
+++ ejercicio_hilos.c
@@ -8,84 +8,91 @@
 #define INF 0
 
 typedef struct {
-    pthread_t thread;
     int sleep_time;
     int x;
-} Hilo;
+} thread_info;
 
-void clean_up(Hilo *h, int *arr, int ini, int end) {
-    int j = 0;
-    int error = 0;
+void clean_up(thread_info *ti, pthread_t *h, int dim) {
+    int i = 0;
+
+    if (ti != NULL) {
+        free(ti);
+    }
+
     if (h != NULL) {
-        for (j = ini; j < end; j++) {
-            error = pthread_cancel(h[j], NULL);
-            if (error) {
-                fprintf(stderr, "pthread_cancel(%d): %s\n", j, strerror(error));
-            }
-        }
+        for (i = 0; i < dim; pthread_cancel(h[i]), i++);
+        for (i = 0; i < dim; pthread_join(h[i], NULL), i++);
         free(h);
     }
-    if (arr != NULL) free(arr);
 }
 
-int* funcion(Hilo h) {
+void* funcion(void* ti) {
     int *ret_val = NULL;
+    thread_info *cast = (thread_info*)ti;
 
+    sleep(cast->sleep_time);
+    
     if ((ret_val = (int*)malloc(sizeof(int))) == NULL) {
         perror("malloc");
         return NULL;
     }
 
+    *ret_val = cast->x * cast->x * cast->x;
 
-    return ret_val;
+    return (void *)ret_val;
 }
 
 int main(int argc, char *argv[]) {
-    int i = 0;
-    int error = 0;
-    int **ret_val = NULL;
-    Hilo *hilos = NULL;
+    int n_hilos = 0, i = 0, error = 0;
+    int *ret_val = NULL;
+    thread_info *info = NULL;
+    pthread_t *hilos = NULL;
 
-    if (argc != 2 || argv[1] < 1) {
-        printf("ERROR: Deberia ser %s <num_hilos> (con num_hilos > 0).\n", argv[0]);
+    if (argc != 2 || (n_hilos = atoi(argv[1])) < 1) {
+        fprintf(stdout, "ERROR: Deberia ser %s <num_hilos> (con num_hilos > 0).\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     /*Reserva de memoria*/
-    if ((hilos = (Hilo*) malloc(argv[1]*sizeof(Hilo))) == NULL) {
-        perror("malloc");
+    if ((info = (thread_info*)malloc(n_hilos*sizeof(thread_info))) == NULL) {
+        perror("ERROR: malloc");
+        clean_up(info, NULL, 0);
         exit(EXIT_FAILURE);
     }
 
-    if ((ret_val = (int**) malloc(argv[1]*sizeof(int*))) == NULL) {
-        perror("malloc");
+    if ((hilos = (pthread_t*)malloc(n_hilos*sizeof(pthread_t))) == NULL) {
+        perror("ERROR: malloc");
+        clean_up(info, hilos, 0);
         exit(EXIT_FAILURE);
     }
 
-    /*Creacion de hilos*/
-    for (i = 0; i < argv[1]; i++) {
-        hilos[i].x = i + 1;
-        hilos[i].sleep_time = (rand()/(RAND_MAX + 1.))*(SUP - INF + 1) + INF;
+    /*Lanzamiento de hilos*/
+    for (i = 0; i < n_hilos; i++) {
+        info[i].sleep_time = rand() % (SUP - INF + 1) + INF;
+        info[i].x = i;
 
-        error = pthread_create(&hilos[i].thread, NULL, funcion, hilos[i]);
-        if (error) {
-            fprintf(stderr, "pthread_create(%d): %s\n", i, strerror(error));
-            clean_up(hilos, ret_val, 0, i - 1);
+        error = pthread_create(&hilos[i], NULL, funcion, (void *)&info[i]);
+        if (error != 0) {
+            fprintf(stderr, "pthread_create: %s\n", strerror(error));
+            clean_up(info, hilos, i);
             exit(EXIT_FAILURE);
         }
     }
 
-    /*Espera de hilos*/
-    for (i = 0; i < argv[1]; i++) {
-        error = pthread_join(hilos[i].thread, &ret_val[i]);
+    /*Recuperacion de retornos*/
+    for (i = 0; i < n_hilos; i++) {
+        error = pthread_join(hilos[i], (void *)&ret_val);
         if (error != 0) {
             fprintf(stderr, "pthread_join: %s\n", strerror(error));
-            clean_up(hilos, ret_val, i);
+            clean_up(info, hilos, n_hilos);
             exit(EXIT_FAILURE);
         }
+
+        fprintf(stdout, "%d\n", *ret_val);
+        free(ret_val);
     }
 
-
+    clean_up(info, hilos, 0);
     printf("El programa %s termino correctamente \n", argv[0]);
     exit(EXIT_SUCCESS);
 }
