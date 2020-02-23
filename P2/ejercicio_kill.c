e1ae6ff07d7f57870c28e43f55c8ca4b86accaa2
leandroGO <leandrogogi19@gmail.com>
2020-02-23 15:02:57 +0100

    Ejercicio 2

---
+++ P2/ejercicio_kill.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int argc, char** argv){
+    int signal, pid;
+    
+    if (argc != 3) {
+        printf("Entrada esperada: %s <signal> <pid>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    signal = atoi(argv[1]);
+    pid = atoi(argv[2]);
+
+    if (kill(pid, signal) == -1) {
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+}
