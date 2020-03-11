7f55e4fb9cee7f967f5b65de7cb5537267ab1258
Fgp910 <f.gutierrezpena910@gmail.com>
2020-03-11 17:32:59 +0100

    Funciona prottemp, hostia

--- P2/ejercicio_prottemp.c
+++ P2/ejercicio_prottemp.c
@@ -6,7 +6,18 @@
 #include <signal.h>
 #include <unistd.h>
 
-void manejador_SIGALRM(int sig) {}
+#define WAIT_N(num_wait) {int i_wait; for (i_wait = 0; i_wait < num_wait; i_wait++) wait(NULL);}
+
+static int global_SIGALRM_flag = 0;
+static int global_SIGUSR2_count = 0;
+
+void manejador_SIGALRM(int sig) {
+    global_SIGALRM_flag = 1;
+}
+
+void manejador_SIGUSR2(int sig) {
+    global_SIGUSR2_count++;
+}
 
 void manejador_SIGTERM(int sig) {
     printf("Finalizado %d\n", getpid());
@@ -27,17 +38,13 @@ void trabajo() {
 }
 
 int main(int argc, char **argv) {
-    int N;
-    int T;
-    int i;
-    int error;
+    int N, T, i, error;
     pid_t ppid = getpid();
     pid_t *hijos;
-    struct sigaction s_alarm;
-    struct sigaction s_term;
+    struct sigaction s_alarm, s_u2, s_term;
     sigset_t block_alarm, wait_alarm;
     sigset_t block_term, wait_term;
-    sigset_t oldset, allset;
+    sigset_t block_u2, oldset;
 
     if (argc != 3 || (N = atoi(argv[1])) < 0 || (T = atoi(argv[2])) < 0) {
         fprintf(stdout, "ERROR: Deberia ser %s <N> <T> (con N,T no negativos).\n", argv[0]);
@@ -54,21 +61,40 @@ int main(int argc, char **argv) {
     s_alarm.sa_flags = 0;
     sigemptyset(&(s_alarm.sa_mask));
 
+    s_u2.sa_handler = manejador_SIGUSR2;
+    s_u2.sa_flags = 0;
+    sigemptyset(&(s_u2.sa_mask));
+
     s_term.sa_handler = manejador_SIGTERM;
     s_term.sa_flags = 0;
     sigemptyset(&(s_term.sa_mask));
 
     sigemptyset(&block_alarm);
     sigemptyset(&block_term);
+    sigemptyset(&block_u2);
     sigaddset(&block_alarm, SIGALRM);
+    sigaddset(&block_u2, SIGUSR2);
     sigaddset(&block_term, SIGTERM);
 
     sigfillset(&wait_alarm);
     sigfillset(&wait_term);
     sigdelset(&wait_alarm, SIGALRM);
+    sigdelset(&wait_alarm, SIGUSR2);    //Permite recibir SIGUSR2 mientras se espera la alarma
     sigdelset(&wait_term, SIGTERM);
-
-    sigfillset(&allset);
+    
+    /*Definiendo manejador_SIGUSR2 como rutina de tratamiento*/
+    if (sigprocmask(SIG_BLOCK, &block_u2, &oldset) < 0) {
+        perror("sigprocmask");
+        exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGUSR2, &s_u2, NULL) < 0) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+    if (sigprocmask(SIG_UNBLOCK, &block_u2, NULL) < 0) {
+        perror("sigprocmask");
+        exit(EXIT_FAILURE);
+    }
 
     /*Creando hijos*/
     for (i = 0; i < N; i++) {
@@ -81,12 +107,12 @@ int main(int argc, char **argv) {
         }
         if (!error) {
             trabajo();
-            /*if (kill(ppid, SIGUSR2) < 0) {
+            if (kill(ppid, SIGUSR2) < 0) {
                 perror("kill");
                 exit(EXIT_FAILURE);
-            }*/
+            }
             /*Definiendo manejador_SIGTERM como rutina de tratamiento*/
-            if (sigprocmask(SIG_BLOCK, &allset, &oldset) < 0) {
+            if (sigprocmask(SIG_BLOCK, &block_term, &oldset) < 0) {
                 perror("sigprocmask");
                 exit(EXIT_FAILURE);
             }
@@ -100,22 +126,28 @@ int main(int argc, char **argv) {
         }
     }
 
-    /*Espera*/
-    if (sigprocmask(SIG_BLOCK, &allset, &oldset) < 0) {
+    /*Definiendo manejador_SIGTERM como rutina de tratamiento*/
+    if (sigprocmask(SIG_BLOCK, &block_alarm, &oldset) < 0) {
         perror("sigprocmask");
+        WAIT_N(N);
         exit(EXIT_FAILURE);
     }
     if (sigaction(SIGALRM, &s_alarm, NULL) < 0) {
         perror("sigaction");
+        WAIT_N(N);
         exit(EXIT_FAILURE);
     }
+    /*Espera*/
     if (alarm(T)) {
         fprintf(stderr, "Existe una alarma previa establecida\n");
     }
-    sigsuspend(&wait_alarm);
+    do {
+        sigsuspend(&wait_alarm);
+    } while (!global_SIGALRM_flag);
 
-    if (sigprocmask(SIG_UNBLOCK, &oldset, NULL) < 0) {
+    if (sigprocmask(SIG_SETMASK, &oldset, NULL) < 0) {
         perror("sigprocmask");
+        WAIT_N(N);
         exit(EXIT_FAILURE);
     }
 
@@ -123,16 +155,14 @@ int main(int argc, char **argv) {
     for (i = 0; i < N; i++) {
         if (kill(hijos[i], SIGTERM) < 0) {
             perror("kill");
-            for (i = 0; i < N; i++)
-                wait(NULL);
+            WAIT_N(N - i);
             exit(EXIT_FAILURE);
         }
     }
 
     /*Salida*/
-    printf("Finalizado padre\n");
+    WAIT_N(N);
+    printf("Finalizado padre (SIGUSR2: %d)\n", global_SIGUSR2_count);
     fflush(stdout);
-    for (i = 0; i < N; i++)
-        wait(NULL);
     exit(EXIT_SUCCESS);
 }
